0021b525b9364156175507e7854e92c8d0a94827
zhoudi <505675592@qq.com>
2016-09-24 15:14:17 +0800

    Android的一些动画

---
+++ app/src/main/java/com/example/zhoudi/animation/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.zhoudi.animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity2 extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main2);
+	}
+	
+}
